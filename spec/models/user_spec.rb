da568671cd1fce590a753fe7276439fdafa98a95
Jsun Pe <jsunpe@gmail.com>
2013-06-08 10:43:07 +1000

    finish sign in out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -87,24 +88,26 @@ describe User do
   describe "email address with mixed case" do 
   	let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
     it "should be saved as all lower-case" do
-	  @user.email = mixed_case_email
-	  @user.save
-	  @user.reload.email.should == mixed_case_email.downcase
-	end 
+  	  @user.email = mixed_case_email
+  	  @user.save
+  	  @user.reload.email.should == mixed_case_email.downcase
+  	end 
   end
 
   describe "return value of authenticate method" do 
   	before { @user.save }
-	let(:found_user) { User.find_by_email(@user.email) }
-	describe "with valid password" do
-	  it { should == found_user.authenticate(@user.password) }
-	end
-	describe "with invalid password" do
-	  let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-	  it { should_not == user_for_invalid_password }
-	  specify { user_for_invalid_password.should be_false } 
-	end
+  	let(:found_user) { User.find_by_email(@user.email) }
+  	describe "with valid password" do
+  	  it { should == found_user.authenticate(@user.password) }
+  	end
+  	describe "with invalid password" do
+  	  let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+  	  it { should_not == user_for_invalid_password }
+  	  specify { user_for_invalid_password.should be_false } 
+  	end
   end
-
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
